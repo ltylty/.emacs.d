44cc522d3d3ef181826ddb2c0a6e32c329ae4028
tianyuan <1137588860@qq.com>
2024-05-09 14:10:09 +0800

    优化字体设置

--- init.el
+++ init.el
@@ -1,13 +1,10 @@
 (require 'package)
-
 ;; (setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
 ;;                          ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
 ;;                          ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
-
 (setq package-archives '(("gnu" . "https://mirrors.ustc.edu.cn/elpa/gnu/")
                          ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")
                          ("nongnu" . "https://mirrors.ustc.edu.cn/elpa/nongnu/")))
-
 (package-initialize)
 
 (use-package ef-themes
@@ -15,7 +12,8 @@
   :config
   (load-theme 'ef-maris-dark :no-confirm))
 
-;; (set-face-attribute 'default nil :font "Maple Mono SC NF")
+(when (member "Maple Mono SC NF" (font-family-list))
+  (set-face-attribute 'default nil :font "Maple Mono SC NF"))
 
 (load-file (expand-file-name "modules/base.el" user-emacs-directory))
 (load-file (expand-file-name "modules/key.el" user-emacs-directory))
