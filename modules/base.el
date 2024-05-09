6095f0ea3b04630136d3ba42476175c10b778fa9
tianyuan <1137588860@qq.com>
2024-05-09 17:44:13 +0800

    add orderless

--- modules/base.el
+++ modules/base.el
@@ -26,10 +26,11 @@
   (setq xref-show-xrefs-function #'consult-xref
         xref-show-definitions-function #'consult-xref)
   :config
-  ;; 解决Windows下 consult-ripgrep 乱码和卡死
-  (add-to-list 'process-coding-system-alist '("[gG][rR][eE][pP]" . (utf-8 . gbk-dos)))
-  (add-to-list 'process-coding-system-alist '("[rR][gG]" . (utf-8 . gbk-dos)))
-  (setq consult-async-input-debounce 0.8))
+  (when (eq system-type 'windows-nt)
+    ;; 解决Windows下 consult-ripgrep 乱码和卡死
+    (add-to-list 'process-coding-system-alist '("[gG][rR][eE][pP]" . (utf-8 . gbk-dos)))
+    (add-to-list 'process-coding-system-alist '("[rR][gG]" . (utf-8 . gbk-dos)))
+    (setq consult-async-input-debounce 0.8)))
 
 (use-package embark
   :ensure t
@@ -58,9 +59,7 @@
   (corfu-quit-at-boundary t)
   (corfu-quit-no-match t) 
   :init
-  (global-corfu-mode)
-  :config
-  (setq completion-styles '(flex)))
+  (global-corfu-mode))
 
 (use-package cape
   :ensure t
@@ -68,3 +67,7 @@
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
   (add-to-list 'completion-at-point-functions #'cape-file))
 
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless flex)))
