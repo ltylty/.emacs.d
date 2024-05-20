25a1a0cd1e7745739401c9551e3f32a6281022c3
liutianyuan <liutianyuan1737@stnts.com>
2024-05-20 18:01:30 +0800

    解决shell中文乱码

--- early-init.el
+++ early-init.el
@@ -2,11 +2,7 @@
 (setq inhibit-startup-message t)
 (setq custom-file (make-temp-file "emacs-custom"))
 (setq-default frame-title-format '("%f    [" mode-name "]"))
-;; (set-language-environment "UTF-8")
-
-;; (set-charset-priority 'unicode)
 (prefer-coding-system 'utf-8)
-;; (setq system-time-locale "C")
 
 (setq default-frame-alist
       '((fullscreen . maximized)
