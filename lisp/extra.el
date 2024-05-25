a119bbc5724adf0876727852529e57a4f48f7451
tianyuan <1137588860@qq.com>
2024-05-25 11:18:46 +0800

    macrursors kbd

--- lisp/extra.el
+++ lisp/extra.el
@@ -12,8 +12,8 @@
     (add-hook 'macrursors-pre-finish-hook mode)
     (add-hook 'macrursors-post-finish-hook mode))
 (define-key evil-normal-state-map (kbd "C-;") 'macrursors-mark-all-lines-or-instances)
-(define-key evil-normal-state-map (kbd "M-p") 'macrursors-mark-previous-instance-of)
-(define-key evil-normal-state-map (kbd "M-n") 'macrursors-mark-next-instance-of)
+(define-key evil-normal-state-map (kbd "M-p") 'macrursors-mark-previous-line)
+(define-key evil-normal-state-map (kbd "M-n") 'macrursors-mark-next-line)
 
 (unless (package-installed-p 'color-rg)
      (package-vc-install "https://github.com/manateelazycat/color-rg"))
