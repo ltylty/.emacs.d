508b95d4ea66bc7e0c91bb4e6b3ccc6161418410
liutianyuan <liutianyuan1737@stnts.com>
2024-05-28 11:54:14 +0800

    lazy load

--- lisp/extra.el
+++ lisp/extra.el
@@ -7,13 +7,22 @@
 
 (unless (package-installed-p 'macrursors)
   (package-vc-install "https://github.com/corytertel/macrursors"))
-(require 'macrursors)
-(dolist (mode '(corfu-mode))
+(use-package macrursors :defer t
+  :config
+  (dolist (mode '(corfu-mode))
     (add-hook 'macrursors-pre-finish-hook mode)
-    (add-hook 'macrursors-post-finish-hook mode))
+    (add-hook 'macrursors-post-finish-hook mode)))
+;; (require 'macrursors)
+;; (dolist (mode '(corfu-mode))
+;;     (add-hook 'macrursors-pre-finish-hook mode)
+;;     (add-hook 'macrursors-post-finish-hook mode))
 
 (unless (package-installed-p 'color-rg)
      (package-vc-install "https://github.com/manateelazycat/color-rg"))
-(require 'color-rg)
-(when (eq system-type 'windows-nt)
-  (setq color-rg-command-prefix "powershell"))
+(use-package color-rg :defer 1
+  :config
+  (when (eq system-type 'windows-nt)
+  (setq color-rg-command-prefix "powershell")))
+;; (require 'color-rg)
+;; (when (eq system-type 'windows-nt)
+;;   (setq color-rg-command-prefix "powershell"))
