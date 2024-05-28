66db2532aacd51b46a50a3d94c0bf039b8f499f8
liutianyuan <liutianyuan1737@stnts.com>
2024-05-28 11:55:34 +0800

    lazy load

--- lisp/extra.el
+++ lisp/extra.el
@@ -12,17 +12,10 @@
   (dolist (mode '(corfu-mode))
     (add-hook 'macrursors-pre-finish-hook mode)
     (add-hook 'macrursors-post-finish-hook mode)))
-;; (require 'macrursors)
-;; (dolist (mode '(corfu-mode))
-;;     (add-hook 'macrursors-pre-finish-hook mode)
-;;     (add-hook 'macrursors-post-finish-hook mode))
 
 (unless (package-installed-p 'color-rg)
      (package-vc-install "https://github.com/manateelazycat/color-rg"))
 (use-package color-rg :defer 1
   :config
   (when (eq system-type 'windows-nt)
-  (setq color-rg-command-prefix "powershell")))
-;; (require 'color-rg)
-;; (when (eq system-type 'windows-nt)
-;;   (setq color-rg-command-prefix "powershell"))
+    (setq color-rg-command-prefix "powershell")))
