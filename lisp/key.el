3c99351b6d4cefd181b94cd25c85de6b31ae1801
liutianyuan <liutianyuan1737@stnts.com>
2024-07-18 14:10:43 +0800

    remove color-rg

--- lisp/key.el
+++ lisp/key.el
@@ -8,7 +8,7 @@
   (setq evil-shift-width 2)
   (setq evil-symbol-word-search t)
   :config
-  (setq evil-emacs-state-modes '(ediff-mode color-rg-mode))
+  (setq evil-emacs-state-modes '(ediff-mode))
   (add-hook 'with-editor-mode-hook 'evil-insert-state)
   (evil-select-search-module 'evil-search-module 'evil-search)
   (evil-mode))
