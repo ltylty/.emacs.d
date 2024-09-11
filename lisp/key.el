e4cacaa99ae19358c9b4c58c61ddd213f33b7023
liutianyuan <liutianyuan1737@stnts.com>
2024-09-11 13:35:30 +0800

    kbd

--- lisp/key.el
+++ lisp/key.el
@@ -67,7 +67,7 @@
   "bi" 'ibuffer
   "bd" 'evil-delete-buffer
   "bl" 'evil-switch-to-windows-last-buffer
-  "bx" #'(lambda () (interactive) (switch-to-buffer (generate-new-buffer-name "scratch")))
+  "bx" #'(lambda () (interactive) (switch-to-buffer "*scratch*"))
   ;; window
   "wd" 'evil-window-delete
   "ww" 'evil-window-next
