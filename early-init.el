2407a2698332842a9b724d1b5414f3b404836791
liutianyuan <liutianyuan1737@stnts.com>
2024-10-08 10:18:30 +0800

    format code

--- early-init.el
+++ early-init.el
@@ -2,14 +2,12 @@
       gc-cons-percentage 0.3
       read-process-output-max (* 10 1024 1024))
 
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 (load-theme 'modus-vivendi-tinted t)
 (prefer-coding-system 'utf-8)
 (setq inhibit-startup-message t)
 (setq initial-major-mode 'fundamental-mode)
 (setq-default frame-title-format '("  %f" (:eval (if (buffer-modified-p) "  ✍️"))))
-
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
 (setq default-frame-alist '((fullscreen . maximized) (font . "Maple Mono SC NF")))
