197c2afbb2cace0a5ecf9bd7da795e910e9db403
liutianyuan <liutianyuan1737@stnts.com>
2024-11-20 19:02:57 +0800

    (setq package-check-signature nil)

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+(setq package-check-signature nil)
 (setq package-archives '(("gnu" . "https://mirrors.ustc.edu.cn/elpa/gnu/")
                          ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")
                          ("nongnu" . "https://mirrors.ustc.edu.cn/elpa/nongnu/")))
