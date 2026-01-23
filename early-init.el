7f03a6de165344432902a61c3d936d2739bc3fad
liutianyuan <liutianyuan1737@stnts.com>
2026-01-23 10:09:17 +0800

    refactor: comment out gc-cons-threshold

--- early-init.el
+++ early-init.el
@@ -1,5 +1,5 @@
 ;; -*- lexical-binding: t; -*-
-(setq gc-cons-threshold (* 100 1024 1024))
+;; (setq gc-cons-threshold (* 100 1024 1024))
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
