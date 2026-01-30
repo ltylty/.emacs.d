2493f1bbac07dc20f051bc756f4c418605530603
liutianyuan <liutianyuan1737@stnts.com>
2026-01-30 12:02:28 +0800

    Remove vterm bindings from key.el\n\n- Removed vterm from evil-insert-state-modes\n- Removed 'ot' key binding for vterm

--- lisp/key.el
+++ lisp/key.el
@@ -12,7 +12,6 @@
   (setq evil-toggle-key "C-M-z")
   :config
   (setq evil-emacs-state-modes '(ediff-mode))
-  (add-to-list 'evil-insert-state-modes 'vterm-mode)
   (add-hook 'with-editor-mode-hook 'evil-insert-state)
   (evil-select-search-module 'evil-search-module 'evil-search)
 
@@ -92,7 +91,6 @@
     "tt" 'tab-recent
     ;; open
     "oe" 'eshell
-    "ot" 'vterm
     "oo" #'(lambda () (interactive) (browse-url default-directory))
     "of" #'(lambda () (interactive) (browse-url buffer-file-name))
     "od" 'dired-jump
