2ba0e18b819b7ac5fadf14a420e02dfcfb198c08
liutianyuan <liutianyuan1737@stnts.com>
2026-04-17 09:55:48 +0800

    fix(dev): add loop detection to vc-dir navigation

--- lisp/dev.el
+++ lisp/dev.el
@@ -37,21 +37,40 @@
   (defun vc-dir-next-and-diff ()
     "移动到下一个文件并显示 diff，跳过文件夹。如果在最后一个文件则跳转到第一个文件。"
     (interactive)
-    (let ((start-pos (point)))
-      (vc-dir-next-line 1)
-      (when (= (point) start-pos)
-        (goto-char (point-min))
-        (vc-dir-next-line 1)))
-    (when (vc-dir-current-should-skip-p)
-      (vc-dir-next-and-diff))
+    (let ((start-node (and vc-ewoc (ewoc-locate vc-ewoc)))
+          (first-visited nil))
+      (catch 'done
+        (while t
+          (vc-dir-next-line 1)
+          (let ((cur-node (and vc-ewoc (ewoc-locate vc-ewoc))))
+            ;; ewoc 节点回环检测：回到起始节点说明已遍历一圈
+            (when (eq cur-node start-node)
+              (if first-visited
+                  (progn (message "No edited files found") (throw 'done nil))
+                (setq first-visited t)
+                (goto-char (point-min))
+                (vc-dir-next-line 1)
+                (setq cur-node (and vc-ewoc (ewoc-locate vc-ewoc)))))
+            (unless (vc-dir-current-should-skip-p)
+              (throw 'done nil))
+            ;; 二次回到起始节点（已 wrap 过）
+            (when (eq cur-node start-node)
+              (message "No edited files found")
+              (throw 'done nil))))))
     (save-selected-window
       (vc-diff)))
   (defun vc-dir-prev-and-diff ()
     "移动到上一个文件并显示 diff，跳过文件夹。"
     (interactive)
-    (vc-dir-previous-line 1)
-    (when (vc-dir-current-should-skip-p)
-      (vc-dir-prev-and-diff))
+    (let ((start-node (and vc-ewoc (ewoc-locate vc-ewoc))))
+      (catch 'done
+        (while t
+          (vc-dir-previous-line 1)
+          (unless (vc-dir-current-should-skip-p)
+            (throw 'done nil))
+          (when (eq (ewoc-locate vc-ewoc) start-node)
+            (message "No edited files found")
+            (throw 'done nil)))))
     (save-selected-window
       (vc-diff)))
   (defun vc-dir-quick-commit-all ()
