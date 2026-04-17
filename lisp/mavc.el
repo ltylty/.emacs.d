bb5ce5285bc428964db17bef7aba1a51d59af8ba
liutianyuan <liutianyuan1737@stnts.com>
2026-04-17 16:13:22 +0800

    refactor(mavc): extract common navigation logic into vc-dir-move-and-diff

--- lisp/mavc.el
+++ lisp/mavc.el
@@ -9,62 +9,47 @@
         (when data
           (or (vc-dir-fileinfo->directory data)
               (eq (vc-dir-fileinfo->state data) 'up-to-date))))))
+
+  (defun vc-dir-move-and-diff (move-fn wrap-pos)
+    "沿 MOVE-FN 方向移动，跳过文件夹和 up-to-date 文件，回环后显示 diff。
+MOVE-FN 为移动函数 (vc-dir-next-line / vc-dir-previous-line)。
+WRAP-POS 为回环目标位置 (point-min / point-max)。"
+    (let ((start-node (and vc-ewoc (ewoc-locate vc-ewoc)))
+          (wrapped nil))
+      (catch 'done
+        (while t
+          (let ((prev-node (and vc-ewoc (ewoc-locate vc-ewoc))))
+            (funcall move-fn 1)
+            (let ((cur-node (and vc-ewoc (ewoc-locate vc-ewoc))))
+              ;; 到达边界无法继续时，回环到另一端
+              (when (and (eq cur-node prev-node) (not wrapped))
+                (setq wrapped t)
+                (goto-char wrap-pos)
+                (funcall move-fn 1)
+                (setq cur-node (and vc-ewoc (ewoc-locate vc-ewoc))))
+              ;; 已遍历一圈回到起始节点
+              (when (eq cur-node start-node)
+                (message "No edited files found")
+                (throw 'done nil))
+              (unless (vc-dir-current-should-skip-p)
+                (throw 'done nil))
+              ;; 回环后仍无法移动
+              (when (eq cur-node prev-node)
+                (message "No edited files found")
+                (throw 'done nil)))))))
+    (save-selected-window
+      (vc-diff)))
+
   (defun vc-dir-next-and-diff ()
     "移动到下一个文件并显示 diff，跳过文件夹。如果在最后一个文件则跳转到第一个文件。"
     (interactive)
-    (let ((start-node (and vc-ewoc (ewoc-locate vc-ewoc)))
-          (wrapped nil))
-      (catch 'done
-        (while t
-          (let ((prev-node (and vc-ewoc (ewoc-locate vc-ewoc))))
-            (vc-dir-next-line 1)
-            (let ((cur-node (and vc-ewoc (ewoc-locate vc-ewoc))))
-              ;; 到达底部无法继续向后时，回环到顶部
-              (when (and (eq cur-node prev-node) (not wrapped))
-                (setq wrapped t)
-                (goto-char (point-min))
-                (vc-dir-next-line 1)
-                (setq cur-node (and vc-ewoc (ewoc-locate vc-ewoc))))
-              ;; 已遍历一圈回到起始节点
-              (when (eq cur-node start-node)
-                (message "No edited files found")
-                (throw 'done nil))
-              (unless (vc-dir-current-should-skip-p)
-                (throw 'done nil))
-              ;; 回环后仍无法移动
-              (when (eq cur-node prev-node)
-                (message "No edited files found")
-                (throw 'done nil)))))))
-    (save-selected-window
-      (vc-diff)))
+    (vc-dir-move-and-diff #'vc-dir-next-line (point-min)))
+
   (defun vc-dir-prev-and-diff ()
     "移动到上一个文件并显示 diff，跳过文件夹。如果在第一个文件则跳转到最后一个文件。"
     (interactive)
-    (let ((start-node (and vc-ewoc (ewoc-locate vc-ewoc)))
-          (wrapped nil))
-      (catch 'done
-        (while t
-          (let ((prev-node (and vc-ewoc (ewoc-locate vc-ewoc))))
-            (vc-dir-previous-line 1)
-            (let ((cur-node (and vc-ewoc (ewoc-locate vc-ewoc))))
-              ;; 到达顶部无法继续向前时，回环到底部
-              (when (and (eq cur-node prev-node) (not wrapped))
-                (setq wrapped t)
-                (goto-char (point-max))
-                (vc-dir-previous-line 1)
-                (setq cur-node (and vc-ewoc (ewoc-locate vc-ewoc))))
-              ;; 已遍历一圈回到起始节点
-              (when (eq cur-node start-node)
-                (message "No edited files found")
-                (throw 'done nil))
-              (unless (vc-dir-current-should-skip-p)
-                (throw 'done nil))
-              ;; 回环后仍无法移动
-              (when (eq cur-node prev-node)
-                (message "No edited files found")
-                (throw 'done nil)))))))
-    (save-selected-window
-      (vc-diff)))
+    (vc-dir-move-and-diff #'vc-dir-previous-line (point-max)))
+
   (defun vc-dir-quick-commit-all ()
     "标记所有已跟踪且修改过的文件并进入提交界面"
     (interactive)
